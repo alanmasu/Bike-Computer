c741b169d158797310f6cd22a0a89d5cde28dd08
alanmasu <alan.masutti@gmail.com>
2023-12-30 13:29:17 +0100

    Used DMA to move 256 bytes from UART EUSCIA2 [TO FIX]
     - It works halfway: the first time it runs, the ISR is triggered, but after not!
        * Mabye we need to clear some other flag?
        * Mabye we have to use a different transaction mode? BOH!

--- main.c
+++ main.c
@@ -35,13 +35,14 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define RX_BUFFER_SIZE 256
+#define RX_BUFFER_SIZE 256                  //!< Size of RX buffer
+                                            //!< Uesed also by DMA as max buffer length
 
-uint8_t TXData = 1;
-uint8_t RXData = 0;
-uint_fast8_t data[RX_BUFFER_SIZE];
-volatile uint_fast16_t dataCount = 0;
-volatile bool stringEnd = false;
+uint8_t TXData = 1;                         //
+uint8_t RXData = 0;                         //
+uint_fast8_t uartData[RX_BUFFER_SIZE];      //!< UART RX buffer
+volatile uint_fast16_t dataCount = 0;       //
+volatile bool stringEnd = false;            //
 
 /* UART Configuration Parameter. These are the configuration parameters to
  * make the eUSCI A UART module to operate with a 115200 baud rate. These
@@ -50,29 +51,29 @@ volatile bool stringEnd = false;
  * http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP430BaudRateConverter/index.html
  */
 const eUSCI_UART_ConfigV1 uartConfig = {
-        EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
-        156,                                      // BRDIV = 13
-        4,                                       // UCxBRF = 0
-        0,                                      // UCxBRS = 37
-        EUSCI_A_UART_NO_PARITY,                  // No Parity
-        EUSCI_A_UART_LSB_FIRST,                  // LSB First
-        EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
-        EUSCI_A_UART_MODE,                       // UART mode
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,             // SMCLK Clock Source
+        156,                                        // BRDIV = 13
+        4,                                          // UCxBRF = 0
+        0,                                          // UCxBRS = 37
+        EUSCI_A_UART_NO_PARITY,                     // No Parity
+        EUSCI_A_UART_LSB_FIRST,                     // LSB First
+        EUSCI_A_UART_ONE_STOP_BIT,                  // One stop bit
+        EUSCI_A_UART_MODE,                          // UART mode
         EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,  // Oversampling
-        EUSCI_A_UART_8_BIT_LEN                  // 8 bit data length
+        EUSCI_A_UART_8_BIT_LEN                      // 8 bit data length
 };
 
 const eUSCI_UART_ConfigV1 pcUartConfig = {
-        EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
-        13,                                      // BRDIV = 13
-        0,                                       // UCxBRF = 0
-        37,                                      // UCxBRS = 37
-        EUSCI_A_UART_NO_PARITY,                  // No Parity
-        EUSCI_A_UART_LSB_FIRST,                  // LSB First
-        EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
-        EUSCI_A_UART_MODE,                       // UART mode
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,             // SMCLK Clock Source
+        13,                                         // BRDIV = 13
+        0,                                          // UCxBRF = 0
+        37,                                         // UCxBRS = 37
+        EUSCI_A_UART_NO_PARITY,                     // No Parity
+        EUSCI_A_UART_LSB_FIRST,                     // LSB First
+        EUSCI_A_UART_ONE_STOP_BIT,                  // One stop bit
+        EUSCI_A_UART_MODE,                          // UART mode
         EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,  // Oversampling
-        EUSCI_A_UART_8_BIT_LEN                  // 8 bit data length
+        EUSCI_A_UART_8_BIT_LEN                      // 8 bit data length
 };
 
 
@@ -90,7 +91,7 @@ static DMA_ControlTable MSP_EXP432P401RLP_DMAControlTable[32];
 
 int main(void){
     /* Halting WDT  */
-    MAP_WDT_A_holdTimer();
+    WDT_A_holdTimer();
 
     /* P1.0 as output (LED) */
     MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
@@ -119,44 +120,78 @@ int main(void){
     // Enable UART module
     MAP_UART_enableModule(EUSCI_A2_BASE);                               //GPS
     MAP_UART_enableModule(EUSCI_A0_BASE);                               //PC
-    // Enabling interrupts
-    MAP_UART_enableInterrupt(EUSCI_A2_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
-    MAP_Interrupt_enableInterrupt(INT_EUSCIA2);
-//    MAP_UART_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
-//    MAP_Interrupt_enableInterrupt(INT_EUSCIA0);
+    // Enabling UART interrupts
+    // MAP_UART_enableInterrupt(EUSCI_A2_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
+    // MAP_Interrupt_enableInterrupt(INT_EUSCIA2);
+    // MAP_UART_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
+    // MAP_Interrupt_enableInterrupt(INT_EUSCIA0);
 
     //DMA
     MAP_DMA_enableModule();
     MAP_DMA_setControlBase(MSP_EXP432P401RLP_DMAControlTable);
     MAP_DMA_assignChannel(DMA_CH5_EUSCIA2RX);
+    /*! 
+        @brief      Set DMA chennel for EUSCI_A2 RX
+        @details    Set DMA chennel for EUSCI_A2 RX to use Primary DMA Mode sets also: 
+                     - 8bit data size 
+                     - source address increment is none, (source is fixed)
+                     - destination address increment is 8bit (destination is incremented by 1)
+                     - arbitration size is 1 (one transfer per request)
+    */
+    DMA_setChannelControl(DMA_CH5_EUSCIA2RX | UDMA_PRI_SELECT,
+                              UDMA_SIZE_8 | UDMA_SRC_INC_NONE | UDMA_DST_INC_8 | UDMA_ARB_1); 
+    /*!
+        @brief      Set DMA chennel transfer parameters for EUSCI_A2 RX
+        @details    Set DMA chennel for EUSCI_A2 RX to use Primary DMA Mode sets also: 
+                     - Basic transfer mode
+                     - Source address
+                     - Destination address
+                     - Transfer size
+    */
+    DMA_setChannelTransfer(DMA_CH5_EUSCIA2RX | UDMA_PRI_SELECT,
+                               UDMA_MODE_BASIC, 
+                               (void *) UART_getReceiveBufferAddressForDMA(EUSCI_A2_BASE),
+                               uartData,
+                               RX_BUFFER_SIZE);
+
+    //Enable DMA interrupts
+    MAP_DMA_assignInterrupt(INT_DMA_INT1, 5);               //!< Assing DMA interrupt 1 to channel 5
+    MAP_DMA_clearInterruptFlag(DMA_CH5_EUSCIA2RX & 0x0F);   //!< Clear interrupt flag for channel 5
+
+    MAP_Interrupt_enableInterrupt(INT_DMA_INT1);            //!< Enable DMA interrupt
+    MAP_DMA_enableInterrupt(INT_DMA_INT1);                  //!< Enable DMA interrupt 1
+    MAP_DMA_enableChannel(5);                               //!< Enable DMA channel 5
+
 
     MAP_Interrupt_enableSleepOnIsrExit();
     while(1){
         if(stringEnd){
-            for(int i = 0; i < dataCount; ++i){
-                MAP_UART_transmitData(EUSCI_A0_BASE, data[i]);
+            for(int i = 0; i < RX_BUFFER_SIZE; ++i){
+                MAP_UART_transmitData(EUSCI_A0_BASE, uartData[i]);
             }
+            stringEnd = false;
+//            MAP_Interrupt_enableInterrupt(INT_DMA_INT1);
+//            MAP_DMA_enableInterrupt(INT_DMA_INT1);
         }
-
-        MAP_Interrupt_enableSleepOnIsrExit();
-        MAP_PCM_gotoLPM0InterruptSafe();
+//        MAP_Interrupt_enableSleepOnIsrExit();
+//        MAP_PCM_gotoLPM0InterruptSafe();
     }
 }
 
 /* EUSCI A0 UART ISR - Echos data back to PC host */
-void EUSCIA2_IRQHandler(void){
-    uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A2_BASE);
+// void EUSCIA2_IRQHandler(void){
+//     uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A2_BASE);
 
-    if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG){
-        RXData = MAP_UART_receiveData(EUSCI_A2_BASE);
-        if(dataCount < RX_BUFFER_SIZE){
-            data[dataCount++] = RXData;
-        }else if(dataCount > RX_BUFFER_SIZE - 1 || (RXData == '\r' && data[dataCount-1] == '\n')){
-            stringEnd = true;
-            MAP_Interrupt_disableSleepOnIsrExit();
-        }
-    }
-}
+//     if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG){
+//         RXData = MAP_UART_receiveData(EUSCI_A2_BASE);
+//         if(dataCount < RX_BUFFER_SIZE){
+//             uartData[dataCount++] = RXData;
+//         }else if(dataCount > RX_BUFFER_SIZE - 1 || (RXData == '\r' && uartData[dataCount-1] == '\n')){
+//             stringEnd = true;
+//             MAP_Interrupt_disableSleepOnIsrExit();
+//         }
+//     }
+// }
 
 //void EUSCIA0_IRQHandler(void){
 //    uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A0_BASE);
@@ -168,3 +203,18 @@ void EUSCIA2_IRQHandler(void){
 //    }
 //
 //}
+
+
+/*! 
+    @brief      DMA interrupt handler and disable interrupts
+*/ 
+void DMA_INT1_IRQHandler(void){
+    stringEnd = true;
+    DMA_clearInterruptFlag(0);
+    DMA_clearInterruptFlag(1);
+
+    /* Disable the interrupt to allow execution */
+    MAP_Interrupt_disableSleepOnIsrExit();
+//    MAP_Interrupt_disableInterrupt(INT_DMA_INT1);
+//    MAP_DMA_disableInterrupt(INT_DMA_INT1);
+}
