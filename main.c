cac88f33ffe481bb1301e215a31c4280e0d178ca
alanmasu <alan.masutti@gmail.com>
2023-12-30 13:20:31 +0100

    Added GPS echo comunication whit ISR and buffer

--- main.c
+++ main.c
@@ -17,10 +17,10 @@
  *               MSP432P401
  *             -----------------
  *            |                 |
- *       RST -|     P3.3/UCA0TXD|----> ESP32 RX
+ *       RST -|     P3.3/UCA0TXD|----> GPS_L80 RX
  *            |                 |
  *           -|                 |
- *            |     P3.2/UCA0RXD|----< ESP32 TX
+ *            |     P3.2/UCA0RXD|----< GPS_L80 TX
  *            |                 |
  *            |             P1.0|---> LED
  *            |                 |
@@ -34,9 +34,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define RX_BUFFER_SIZE 256
+
 uint8_t TXData = 1;
 uint8_t RXData = 0;
-uint_fast8_t data[256];
+uint_fast8_t data[RX_BUFFER_SIZE];
+volatile uint_fast16_t dataCount = 0;
+volatile bool stringEnd = false;
 
 /* UART Configuration Parameter. These are the configuration parameters to
  * make the eUSCI A UART module to operate with a 115200 baud rate. These
@@ -45,6 +49,19 @@ uint_fast8_t data[256];
  * http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP430BaudRateConverter/index.html
  */
 const eUSCI_UART_ConfigV1 uartConfig = {
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
+        156,                                      // BRDIV = 13
+        4,                                       // UCxBRF = 0
+        0,                                      // UCxBRS = 37
+        EUSCI_A_UART_NO_PARITY,                  // No Parity
+        EUSCI_A_UART_LSB_FIRST,                  // LSB First
+        EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
+        EUSCI_A_UART_MODE,                       // UART mode
+        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,  // Oversampling
+        EUSCI_A_UART_8_BIT_LEN                  // 8 bit data length
+};
+
+const eUSCI_UART_ConfigV1 pcUartConfig = {
         EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
         13,                                      // BRDIV = 13
         0,                                       // UCxBRF = 0
@@ -61,9 +78,7 @@ int main(void){
     /* Halting WDT  */
     MAP_WDT_A_holdTimer();
 
-    /* Selecting P1.2 and P1.3 in UART mode and P1.0 as output (LED) */
-    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P3,
-             GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+    /* P1.0 as output (LED) */
     MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
     MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
     //Set RGB led pins as output
@@ -77,38 +92,60 @@ int main(void){
     MAP_PCM_setCoreVoltageLevel(PCM_VCORE1);
     CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_24);
 
-    /* Configuring UART Module */
-    MAP_UART_initModule(EUSCI_A2_BASE, &uartConfig);
 
-    /* Enable UART module */
-    MAP_UART_enableModule(EUSCI_A2_BASE);
-
-    /* Enabling interrupts */
+    /* Configuring UART Modules */
+    //Setting pins
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P3,
+                GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);  //GPS
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
+                GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);   //PC
+    //Configure module
+    MAP_UART_initModule(EUSCI_A2_BASE, &uartConfig);                    //GPS
+    MAP_UART_initModule(EUSCI_A0_BASE, &pcUartConfig);                  //PC
+    // Enable UART module
+    MAP_UART_enableModule(EUSCI_A2_BASE);                               //GPS
+    MAP_UART_enableModule(EUSCI_A0_BASE);                               //PC
+    // Enabling interrupts
     MAP_UART_enableInterrupt(EUSCI_A2_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
     MAP_Interrupt_enableInterrupt(INT_EUSCIA2);
+//    MAP_UART_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
+//    MAP_Interrupt_enableInterrupt(INT_EUSCIA0);
     MAP_Interrupt_enableSleepOnIsrExit();
-    MAP_UART_transmitData(EUSCI_A2_BASE, 's');
+
     while(1){
-        for(int i = 0; i < 256; ++i){
-            MAP_UART_transmitData(EUSCI_A2_BASE, data[i]);
+        if(stringEnd){
+            for(int i = 0; i < dataCount; ++i){
+                MAP_UART_transmitData(EUSCI_A0_BASE, data[i]);
+            }
         }
 
         MAP_Interrupt_enableSleepOnIsrExit();
         MAP_PCM_gotoLPM0InterruptSafe();
     }
 }
-uint_fast16_t i = 0;
+
 /* EUSCI A0 UART ISR - Echos data back to PC host */
 void EUSCIA2_IRQHandler(void){
     uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A2_BASE);
 
     if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG){
         RXData = MAP_UART_receiveData(EUSCI_A2_BASE);
-        if(i < 256){
-            data[i++] = RXData;
-        }else{
+        if(dataCount < RX_BUFFER_SIZE){
+            data[dataCount++] = RXData;
+        }else if(dataCount > RX_BUFFER_SIZE - 1 || (RXData == '\r' && data[dataCount-1] == '\n')){
+            stringEnd = true;
             MAP_Interrupt_disableSleepOnIsrExit();
         }
     }
-
 }
+
+//void EUSCIA0_IRQHandler(void){
+//    uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A0_BASE);
+//
+//
+//    if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG){
+//        uint_fast8_t data = MAP_UART_receiveData(EUSCI_A0_BASE);
+//        MAP_UART_transmitData(EUSCI_A0_BASE, data);
+//    }
+//
+//}
