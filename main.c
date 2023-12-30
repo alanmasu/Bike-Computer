c519761a8d455d7524693c8f79399c59139cb9e9
alanmasu <alan.masutti@gmail.com>
2023-12-30 13:20:31 +0100

    Added SDK UART Loopback example and modified fror interfacing whit ESP32 [TESTING]
     Modified example for use an UART interface:
      - Used an ESP32 as interpreter and genarator of Uart packets
      - Used MSP432 for generating the start command and echo back the generated data
        by the ESP

--- main.c
+++ main.c
@@ -1,11 +1,107 @@
-#include <ti/devices/msp432p4xx/inc/msp.h>
+/******************************************************************************
+ * MSP432 UART - Loopback with 24MHz DCO BRCLK
+ *
+ * Description:
+ * I'm testing UART connections and implementation using DriverLib
+ *
+ * This demo connects TX to ESP32 RX and RX to ESP32 TX,
+ *  The example code shows proper initialization of registers
+ *  and interrupts to receive and transmit data.
+ *
+ *  ESP32 is programmed for printing numbers from 0 to 255 when it receives 's' character
+ *  and echo on the USB Virtual COM all packets that's receives back
+ *
+ *
+ *  MCLK = HSMCLK = SMCLK = DCO of 24MHz
+ *
+ *               MSP432P401
+ *             -----------------
+ *            |                 |
+ *       RST -|     P3.3/UCA0TXD|----> ESP32 RX
+ *            |                 |
+ *           -|                 |
+ *            |     P3.2/UCA0RXD|----< ESP32 TX
+ *            |                 |
+ *            |             P1.0|---> LED
+ *            |                 |
+ *
+ *******************************************************************************/
+/* DriverLib Includes */
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+//#include <ti/devices/msp432p4xx/inc/msp.h>
 
+/* Standard Includes */
+#include <stdint.h>
+#include <stdbool.h>
 
-/**
- * main.c
+uint8_t TXData = 1;
+uint8_t RXData = 0;
+
+/* UART Configuration Parameter. These are the configuration parameters to
+ * make the eUSCI A UART module to operate with a 115200 baud rate. These
+ * values were calculated using the online calculator that TI provides
+ * at:
+ * http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP430BaudRateConverter/index.html
  */
-void main(void){
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+const eUSCI_UART_ConfigV1 uartConfig ={
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
+        13,                                      // BRDIV = 13
+        0,                                       // UCxBRF = 0
+        37,                                      // UCxBRS = 37
+        EUSCI_A_UART_NO_PARITY,                  // No Parity
+        EUSCI_A_UART_LSB_FIRST,                  // LSB First
+        EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
+        EUSCI_A_UART_MODE,                       // UART mode
+        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,  // Oversampling
+        EUSCI_A_UART_8_BIT_LEN                  // 8 bit data length
+};
+
+int main(void){
+    /* Halting WDT  */
+    MAP_WDT_A_holdTimer();
+
+    /* Selecting P1.2 and P1.3 in UART mode and P1.0 as output (LED) */
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P3,
+             GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+    MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    //Set RGB led pins as output
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0);
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN1);
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN2);
+
+    /* Setting DCO to 24MHz (upping Vcore) */
+    FlashCtl_setWaitState(FLASH_BANK0, 1);
+    FlashCtl_setWaitState(FLASH_BANK1, 1);
+    MAP_PCM_setCoreVoltageLevel(PCM_VCORE1);
+    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_24);
+
+    /* Configuring UART Module */
+    MAP_UART_initModule(EUSCI_A2_BASE, &uartConfig);
+
+    /* Enable UART module */
+    MAP_UART_enableModule(EUSCI_A2_BASE);
+
+    /* Enabling interrupts */
+    MAP_UART_enableInterrupt(EUSCI_A2_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
+    MAP_Interrupt_enableInterrupt(INT_EUSCIA2);
+    MAP_Interrupt_enableSleepOnIsrExit();
+    MAP_UART_transmitData(EUSCI_A2_BASE, 's');
+    while(1){
+        UART_transmitData(EUSCI_A2_BASE, RXData);
+
+        MAP_Interrupt_enableSleepOnIsrExit();
+        MAP_PCM_gotoLPM0InterruptSafe();
+    }
+}
+
+/* EUSCI A0 UART ISR - Echos data back to PC host */
+void EUSCIA2_IRQHandler(void){
+    uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A2_BASE);
+
+    if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG){
+        RXData = MAP_UART_receiveData(EUSCI_A2_BASE);
+        MAP_Interrupt_disableSleepOnIsrExit();
+    }
 
 }
