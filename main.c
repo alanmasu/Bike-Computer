733356c753c37ac75531aa9ccece704acd15f9e7
Sofiaz17 <sofia.zandona@studenti.unitn.it>
2024-01-05 15:52:01 +0100

    first try of code for counting wheel turns [NOT COMPILED][TO TEST]

--- main.c
+++ main.c
@@ -2,14 +2,85 @@
  *  @brief Main function for the program
  *	@details This file contains the main function for the program
  *	@version 1.0
- *	@date 03/01/2024
+ *	@date 05/01/2024
  */
-
-#include <ti/devices/msp432p4xx/inc/msp.h>
+/* DriverLib Includes */
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
+/* Standard Includes */
+#include <stdint.h>
+#define NUMBER_TIMER_CAPTURES       20
+#define WHEEL_CIRCUMFERENCE         2.3141      //metri
 
-void main(void){
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+/* Timer_A Up Mode Configuration Parameter */
+const Timer_A_UpModeConfig upModeConfig =
+{
+        TIMER_A_CLOCKSOURCE_ACLK,               //frequency: 32.768 kHz
+        TIMER_A_CLOCKSOURCE_DIVIDER_64,         //new frequency: 512 Hz
+        512,                                    //timer counts 512 rising edges of the clock: 1s (qualsiasi sia questo valore, è importante contare sempre fino a 1s)
+        TIMER_A_TAIE_INTERRUPT_ENABLE,
+        TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE,
+        TIMER_A_SKIP_CLEAR
+};
+
+/* Timer_A Capture Mode Configuration Parameter */
+const Timer_A_CaptureModeConfig captureModeConfig =
+{
+        TIMER_A_CAPTURECOMPARE_REGISTER_1,        // CC Register 1
+        TIMER_A_CAPTUREMODE_RISING_EDGE,          // Rising Edge
+        TIMER_A_CAPTURE_INPUTSELECT_CCIxB,        // CCIxB Input Select
+        TIMER_A_CAPTURE_SYNCHRONOUS,              // Synchronized Capture
+        TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE,  // Enable interrupt
+        TIMER_A_OUTPUTMODE_OUTBITVALUE            // Output bit value
+};
+
+/* Statics */
+static volatile uint_fast16_t timerAcaptureValues[NUMBER_TIMER_CAPTURES];
+static volatile uint32_t timerAcapturePointer = 0;
+
+int main(void)
+{
+    /* Stop watchdog timer */
+    MAP_WDT_A_holdTimer();
+
+    /* Configuring P6.1 as peripheral input for capture (sensor) */
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P6, GPIO_PIN1,
+            GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Configuring Capture Mode */
+    MAP_Timer_A_initCapture(TIMER_A0_BASE, &captureModeConfig);
+
+    /* Configuring Up Mode */
+    MAP_Timer_A_configureUpMode(TIMER_A0_BASE, &upModeConfig);
+
+    /* Enabling interrupts and going to sleep */
+    MAP_Interrupt_enableSleepOnIsrExit();
+    MAP_Interrupt_enableInterrupt(INT_TA0_N);
+    MAP_Interrupt_enableMaster();
+
+    /* Starting the Timer_A0 in up mode */
+    MAP_Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
+
+    MAP_PCM_gotoLPM0();
 
 }
+
+//******************************************************************************
+//
+//This is the TIMERA interrupt vector service routine.
+//
+//******************************************************************************
+void TA0_N_IRQHandler(void)
+{
+        uint_fast16_t capturedValue = MAP_Timer_A_getCaptureCompareCount(TIMER_A0_BASE,
+                                             TIMER_A_CAPTURECOMPARE_REGISTER_1);
+
+        if (timerAcapturePointer < NUMBER_TIMER_CAPTURES)
+        {
+                timerAcaptureValues[timerAcapturePointer++] = capturedValue;    //metri al secondo
+                        //salva 20 valori (quindi i m/s di 20 secondi consecutivi)
+    }
+
+
+}
+
