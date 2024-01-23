8968a1fe559aae4aad08eaac8288521fe1c8c495
Federica_Lorenzini <kikalore2002@gmail.com>
2024-01-23 15:51:19 +0100

    used snprintf to print strings on lcd screen

--- main.c
+++ main.c
@@ -9,13 +9,26 @@
 int main(void){
     graphicsInit();
     drawGrid();
+    char tmpString[40]="/0";
 
-    GrStringDraw(&g_sContext,(int8_t*)StringTemp(98),-1, temp.xMin + 60, temp.yMin + 4, 0);
-    GrStringDraw(&g_sContext,"Avg Speed: ",-1,avSpeed.xMin+7,avSpeed.yMin+2,0);
-    GrStringDraw(&g_sContext,"Inst Speed: ",-1,instSpeed.xMin+7,instSpeed.yMin+2,0);
-    GrStringDraw(&g_sContext,"Acc: ",-1,acc.xMin+7,acc.yMin+2,0);
-    GrStringDraw(&g_sContext,"Distance: ",-1,distance.xMin+7,distance.yMin+2,0);
-    GrStringDraw(&g_sContext,"GpsCoord&Altitude:",-1,coord.xMin+7,coord.yMin+2,0);
+    snprintf(tmpString,39,"Temp: %.2f C",22.6);
+    GrStringDraw(&g_sContext,(int8_t*)tmpString,-1, temp.xMin + 60, temp.yMin + 4, 0);
+
+    snprintf(tmpString,39,"AvgSpeed: %.2f km/h",24.5);
+    GrStringDraw(&g_sContext,(int8_t*)tmpString,-1,avSpeed.xMin+7,avSpeed.yMin+2,0);
+
+    snprintf(tmpString,39,"Speed: %.2f km/h",34.5);
+    GrStringDraw(&g_sContext,(int8_t*)tmpString,-1,instSpeed.xMin+7,instSpeed.yMin+4,0);
+
+    snprintf(tmpString,39,"Acc: %.2f m/s^2",20.6);
+    GrStringDraw(&g_sContext,(int8_t*)tmpString,-1,acc.xMin+7,acc.yMin+2,0);
+
+    snprintf(tmpString,39,"Distance: %.2f km",20.0);
+    GrStringDraw(&g_sContext,(int8_t*)tmpString,-1,distance.xMin+7,distance.yMin+2,0);
+
+    snprintf(tmpString,39,"Gps Altitude:%d m",600);
+    GrStringDraw(&g_sContext,(int8_t*)tmpString,-1,coord.xMin+7,coord.yMin+2,0);
+
     GrStringDrawCentered(&g_sContext,"GO TO MENU",-1,(menu.xMax)/2,109,0);
     GrFlush(&g_sContext);
 
